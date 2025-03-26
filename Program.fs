197803ec86ca8f0f42275992e7b028018e0f9de1
Alexey Tolstopyatov <att33pod@gmail.com>
2025-03-26 14:12:01 +0700

    .net 7 -> .net 6

--- Program.fs
+++ Program.fs
@@ -50,7 +50,7 @@ let replaceSymbols (map: char[,]) (choice: int) (symbol: char) : char[,] =
 let generateWaterBlocks (map: char[,]) (choice: int) : char[,] =
     let random = Random()
 
-    for _ in 1 .. choice do
+    for _ in 1..choice do
         let blockWidth = random.Next(2, 10) // Adjust the range of the width as desired
         let blockHeight = random.Next(2, 10) // Adjust the range of the height as desired
         let x = random.Next(blockWidth, Array2D.length1 map - blockWidth)
@@ -59,10 +59,34 @@ let generateWaterBlocks (map: char[,]) (choice: int) : char[,] =
         for i in -blockWidth .. blockWidth do
             for j in -blockHeight .. blockHeight do
                 if (i * i * blockHeight * blockHeight) + (j * j * blockWidth * blockWidth) <= (blockWidth * blockWidth * blockHeight * blockHeight) then
-                    map.[x + i, y + j] <- ' '
+                    map.[x + i, y + j] <- '^' // water block
 
     map
 
+/// Generates water blocks in the map,
+/// replacing '0' elements with oval blocks
+/// Parameters:
+///     map: char[,] - Map to be modified
+///     choice: int Number of water blocks
+let generateWaterBlocksManyInstances (map: char[,]) (choice: int) =
+    let wrInst = Random()
+    let hrInst = Random()
+    let bWidth: int = wrInst.Next (2, 10)
+    let bHeight: int = hrInst.Next (2, 10)
+    
+    let bSHeight = bHeight * bHeight
+    let bSWidth = bWidth * bWidth
+    
+    for _ in 1..choice do
+        let x = wrInst.Next (wrInst.Next (2, 10), Array2D.length1 map - wrInst.Next(2, 10))
+        let y = hrInst.Next (hrInst.Next (2, 10), Array2D.length2 map - hrInst.Next(2, 10))
+
+        for i in -bWidth .. bWidth do
+            for j in -bHeight .. bHeight do
+                if (i * i * bSHeight) + (j * j * bSWidth) <= (bSHeight * bSWidth) then
+                    map.[x + i, y + j] <- '^' // water block
+    
+    map
 
 /// Generates tree blocks on the map by replacing '0' symbols.
 /// Parameters:
@@ -71,15 +95,15 @@ let generateWaterBlocks (map: char[,]) (choice: int) : char[,] =
 /// Returns:
 /// - char[,]: The modified map with tree blocks.
 let generateTreeBlocks (map: char[,]) (choice: int) : char[,] =
-    let random = Random()
-
     for _ in 1 .. choice do
-        let x = random.Next(0, Array2D.length1 map)
-        let y = random.Next(0, Array2D.length2 map)
+        let x = Random.Shared.Next(0, Array2D.length1 map)
+        let y = Random.Shared.Next(0, Array2D.length2 map)
 
         map.[x, y] <- 'T'
-
     map
+    
+let compress unit =
+    ()
 
 [<EntryPoint>]
 let main argv =
